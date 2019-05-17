3a392c5dbdbf75e052aa8b2982e571c2c6a4814a
guozhangli <364627306@qq.com>
2019-05-17 13:10:30 +0800

    test

--- src/main/java/com/example/Demo4Application.java
+++ src/main/java/com/example/Demo4Application.java
@@ -2,12 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo4Application {
+public class Demo4Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo4Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Demo4Application.class);
     }
 
 }
